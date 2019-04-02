12113b8c055a3090886087861462ef7d2bdc141d
Celestialdeath99 <Schmitzbrandan0@gmail.com>
2019-04-02 01:09:17 -0500

    Add initial check for previous run.
    Checks the boolea key value called "seen" stored in the platform keystore. If true, it loads shows the home screen, if false it shows what will become the Welcome/First Launch screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  Widget _defaultHome = new WelcomeScreen();
+  bool _seen = false;
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+  SharedPreferences preferences = await SharedPreferences.getInstance();
+  _seen = (preferences.get('seen') ?? false);
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  if (_seen) {
+    _defaultHome = new HomeScreen();
   }
 
+  runApp(new MaterialApp(
+    title: 'Proxcontrol',
+    home: _defaultHome,
+    theme: new ThemeData(
+      primarySwatch: Colors.indigo,
+      accentColor: Colors.brown,
+    ),
+  ));
+}
+
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Home'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+        child: Text('VM listig will go here by default.'),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GridView.count(
+        primary: false,
+        padding: EdgeInsets.all(MediaQuery.of(context).size.width * 1/12),
+        crossAxisCount: 1,
+        children: <Widget>[
+          Text('Welcome to Proxcontrol', textAlign: TextAlign.center, style: TextStyle(fontSize: 34, fontWeight: FontWeight.w400, letterSpacing: 0.25), textScaleFactor: 1),
+          RaisedButton(
+                child: Text('GET STARTED', style: TextStyle(fontSize: 14, letterSpacing: 1.25, fontWeight: FontWeight.w500)),
+                onPressed: () {
+                  _disableFirstStartup();
+                  Navigator.of(context).pushReplacement(new MaterialPageRoute(
+                      builder: (context) => new HomeScreen()));
+                }
+          )
+        ],
+      ),
+    );
+  }
+
+  Future _disableFirstStartup() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    preferences.setBool('seen', true);
   }
 }
